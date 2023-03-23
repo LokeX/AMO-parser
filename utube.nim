02ffcf1f9a14ab9acd602f1ec89580c30b44a77b
LokeX <pernielsen117@gmail.com>
2023-03-24 00:56:04 +0100

    optimized parsing

--- utube.nim
+++ utube.nim
@@ -5,6 +5,7 @@ import sequtils
 import os
 
 const channelsFile = "channels.txt"
+type RssEntry = tuple[header,name,time,url:string]
 
 func channelUrl(channel:string):string =
   "https://www.youtube.com/"&channel&"/videos"
@@ -19,7 +20,7 @@ func parseUrl(channelContent,urlId:string):string =
 proc fileLines(prm:string):string =
   if fileExists(channelsFile):
     for line in lines(channelsFile): result.add line&"\n"
-    if result.find(prm) == -1: result.add prm&"\n"
+  if result.find(prm) == -1: result.add prm&"\n"
 
 proc paramChannel(default:string):string =
   for param in commandLineParams():
@@ -31,13 +32,25 @@ proc paramChannel(default:string):string =
       return param
   default
 
-func parseEntries(rssLines:openArray[string]):(seq[string],seq[string]) =
-  for line in rssLines[7..rssLines.high]:
-    let l = line.strip
-    if l.startsWith("<title>"):
-      result[0].add l["<title>".len..l.find("<",6)-1]
-    elif l.startsWith("<link"):
-      result[1].add l[l.find("http")..l.find('>')-3]
+func parseEntries(rssLines:openArray[string],max:int):seq[RssEntry] =
+  var entries:array[4,seq[string]]
+  for line in rssLines[9..rssLines.high]:
+    let ls = line.strip
+    if ls.startsWith("<title>"):
+      entries[0].add ls["<title>".len..ls.find("<",6)-1]
+    elif ls.startsWith("<name>"):
+      entries[1].add ls["<name>".len..ls.find("<",6)-1]
+    elif ls.startsWith("<updated>"):
+      entries[2].add ls["<updated>".len..ls.find("T",6)-1]
+    elif ls.startsWith("<link"):
+      entries[3].add ls[ls.find("http")..ls.find('>')-3]
+    let e = entries.mapIt(it.len).deduplicate
+    if max > 0 and e.len == 1 and e[0] == max: break
+  for idx in 0..entries[entries.mapIt(it.len).minIndex].high:
+    result.add (
+      entries[0][idx],entries[1][idx],
+      entries[2][idx],entries[3][idx]
+    )
 
 proc channelsUrls(fileName:string):seq[string] =
   for line in lines(fileName): result.add line.channelUrl
@@ -48,25 +61,23 @@ proc urlFuture(url:string):Future[string] {.async.} =
 proc urlsContent(urls:openArray[string]):seq[string] =
   waitFor all urls.mapIt(it.urlFuture)
 
-func zipTuple[T,U](x:(seq[T],seq[U])):seq[(T,U)] = zip(x[0],x[1])
-
-proc allLatestChannelsEntries(fileName:string):seq[(string,string)] =
+proc allLatestChannelsEntries(fileName:string):seq[RssEntry] =
   channelsUrls(fileName)
   .urlsContent()
   .mapIt(it.parseUrl("rssUrl"))
   .urlsContent()
-  .mapIt(parseEntries(it.splitLines).zipTuple[0])
+  .mapIt(it.splitLines.parseEntries(1)[0])
 
-proc allChannelEntries(prmChannel:string):seq[(string,string)] =
+proc allChannelEntries(prmChannel:string):seq[RssEntry] =
   let
     http = newHttpClient()
     channelContent = http.getContent channelUrl prmChannel
     channelRssUrl = channelContent.parseUrl("rssUrl")
     rssLines = http.getContent(channelRssUrl).splitLines
   echo channelRssUrl  
-  rssLines.parseEntries.zipTuple
+  rssLines.parseEntries(-1)
 
-proc write(channelEntries:seq[(string,string)])
+proc write(channelEntries:seq[RssEntry])
 
 let prmChannel = paramChannel("channelsFile")
 if prmChannel == "channelsFile":
@@ -75,7 +86,9 @@ else:
   write allChannelEntries(prmChannel)
 
 import terminal
-proc write(channelEntries:seq[(string,string)]) =
-  for (header,url) in channelEntries:
-    stdout.styledWriteLine fgYellow,header
-    stdout.styledWriteLine fgMagenta,url
+proc write(channelEntries:seq[RssEntry]) =
+  for entry in channelEntries:
+    stdout.styledWrite fgYellow,entry.name&": "
+    stdout.styledWrite fgBlue,entry.time&": "
+    stdout.styledWriteLine fgYellow,entry.header
+    stdout.styledWriteLine fgMagenta,entry.url
